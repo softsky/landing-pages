385dac3aa62338b99ad08731f5cc5d657c3d16be
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-25 06:21:41 +0200

    initial commit, translation tag attributes added, some styling

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+
+import { QuoteService } from './quote.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  quote: string;
+  isLoading: boolean;
+
+  @ViewChild('loaderWrapper') loaderWrapper:ElementRef;
+
+  constructor(private quoteService: QuoteService) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.quoteService.getRandomQuote({ category: 'dev' })
+      .pipe(finalize(() => { this.isLoading = false; }))
+      .subscribe((quote: string) => { this.quote = quote; });
+
+  }
+
+  ngAfterViewInit() {
+  }
+
+}
